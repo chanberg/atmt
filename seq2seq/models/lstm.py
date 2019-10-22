2e359e91a98a9244ae30015af61a1fcb1619dc7e
chanberg <chantal.amrhein@bluewin.ch>
2019-10-22 07:58:37 +0200

    add comments for lexical model implementation

--- seq2seq/models/lstm.py
+++ seq2seq/models/lstm.py
@@ -35,6 +35,7 @@ class LSTMModel(Seq2SeqModel):
         parser.add_argument('--decoder-dropout-in', type=float, help='dropout probability for decoder input embedding')
         parser.add_argument('--decoder-dropout-out', type=float, help='dropout probability for decoder output')
         parser.add_argument('--decoder-use-attention', help='decoder attention')
+        parser.add_argument('--decoder-use-lexical-model', help='toggle for the lexical model')
 
     @classmethod
     def build_model(cls, args, src_dict, tgt_dict):
@@ -67,7 +68,8 @@ class LSTMModel(Seq2SeqModel):
                               dropout_in=args.decoder_dropout_in,
                               dropout_out=args.decoder_dropout_out,
                               pretrained_embedding=decoder_pretrained_embedding,
-                              use_attention=bool(eval(args.decoder_use_attention)))
+                              use_attention=bool(eval(args.decoder_use_attention)),
+                              use_lexical_model=bool(eval(args.decoder_use_lexical_model)))
         return cls(encoder, decoder)
 
 
@@ -185,7 +187,8 @@ class LSTMDecoder(Seq2SeqDecoder):
                  dropout_in=0.25,
                  dropout_out=0.25,
                  pretrained_embedding=None,
-                 use_attention=True):
+                 use_attention=True,
+                 use_lexical_model=False):
 
         super().__init__(dictionary)
 
@@ -209,12 +212,19 @@ class LSTMDecoder(Seq2SeqDecoder):
 
         self.final_projection = nn.Linear(hidden_size, len(dictionary))
 
+        self.use_lexical_model = use_lexical_model
+        if self.use_lexical_model:
+            # __LEXICAL: Add parts of decoder architecture corresponding to the LEXICAL MODEL here
+            pass
+            # TODO: --------------------------------------------------------------------- /CUT
+
     def forward(self, tgt_inputs, encoder_out, incremental_state=None):
         """ Performs the forward pass through the instantiated model. """
         # Optionally, feed decoder input token-by-token
         if incremental_state is not None:
             tgt_inputs = tgt_inputs[:, -1:]
 
+        # __LEXICAL: Following code is to assist with the LEXICAL MODEL implementation
         # Recover encoder input
         src_embeddings = encoder_out['src_embeddings']
 
@@ -243,6 +253,10 @@ class LSTMDecoder(Seq2SeqDecoder):
         attn_weights = tgt_embeddings.data.new(batch_size, tgt_time_steps, src_time_steps).zero_()
         rnn_outputs = []
 
+        # __LEXICAL: Following code is to assist with the LEXICAL MODEL implementation
+        # Cache lexical context vectors per translation time-step
+        lexical_contexts = []
+
         for j in range(tgt_time_steps):
             # Concatenate the current token embedding with output from previous time step (i.e. 'input feeding')
             lstm_input = torch.cat([tgt_embeddings[j, :, :], input_feed], dim=1)
@@ -261,6 +275,12 @@ class LSTMDecoder(Seq2SeqDecoder):
                 input_feed, step_attn_weights = self.attention(tgt_hidden_states[-1], src_out, src_mask)
                 attn_weights[:, j, :] = step_attn_weights
 
+                if self.use_lexical_model:
+                    # __LEXICAL: Compute and collect LEXICAL MODEL context vectors here
+                    # TODO: --------------------------------------------------------------------- CUT
+                    pass
+                    # TODO: --------------------------------------------------------------------- /CUT
+
             input_feed = F.dropout(input_feed, p=self.dropout_out, training=self.training)
             rnn_outputs.append(input_feed)
 
@@ -276,6 +296,12 @@ class LSTMDecoder(Seq2SeqDecoder):
 
         # Final projection
         decoder_output = self.final_projection(decoder_output)
+
+        if self.use_lexical_model:
+            # __LEXICAL: Incorporate the LEXICAL MODEL into the prediction of target tokens here
+            pass
+            # TODO: --------------------------------------------------------------------- /CUT
+
 
         return decoder_output, attn_weights
 
@@ -297,3 +323,4 @@ def base_architecture(args):
     args.decoder_dropout_in = getattr(args, 'decoder_dropout_in', 0.25)
     args.decoder_dropout_out = getattr(args, 'decoder_dropout_out', 0.25)
     args.decoder_use_attention = getattr(args, 'decoder_use_attention', 'True')
+    args.decoder_use_lexical_model = getattr(args, 'decoder_use_lexical_model', 'False')
