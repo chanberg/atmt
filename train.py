55724b448e299bbc2267c9acf7b8d73bef2327b9
Chantal Amrhein <chantal.amrhein@bluewin.ch>
2020-09-18 17:39:10 +0200

    make French-English default pair and reduce early stopping patience

--- train.py
+++ train.py
@@ -21,7 +21,7 @@ def get_args():
 
     # Add data arguments
     parser.add_argument('--data', default='indomain/prepared_data', help='path to data directory')
-    parser.add_argument('--source-lang', default='de', help='source language')
+    parser.add_argument('--source-lang', default='fr', help='source language')
     parser.add_argument('--target-lang', default='en', help='target language')
     parser.add_argument('--max-tokens', default=None, type=int, help='maximum number of tokens in a batch')
     parser.add_argument('--batch-size', default=1, type=int, help='maximum number of sentences in a batch')
@@ -34,7 +34,7 @@ def get_args():
     parser.add_argument('--max-epoch', default=10000, type=int, help='force stop training at specified epoch')
     parser.add_argument('--clip-norm', default=4.0, type=float, help='clip threshold of gradients')
     parser.add_argument('--lr', default=0.0003, type=float, help='learning rate')
-    parser.add_argument('--patience', default=5, type=int,
+    parser.add_argument('--patience', default=3, type=int,
                         help='number of epochs without improvement on validation set before early stopping')
 
     # Add checkpoint arguments
